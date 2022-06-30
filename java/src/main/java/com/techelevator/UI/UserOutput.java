edeb6e2bb9b9bd7be599db3b85c5bbc2913d9e0d
Mohamed Fofana <mohamed.fofana63@yahoo.com>
2022-06-30 11:54:42 -0400

    added a displaySalesReport method

--- java/src/main/java/com/techelevator/UI/UserOutput.java
+++ java/src/main/java/com/techelevator/UI/UserOutput.java
@@ -1,16 +1,18 @@
 package com.techelevator.UI;
 
 import com.techelevator.Inventory.Inventory;
+import com.techelevator.application.Bank;
+import com.techelevator.Finance.SalesReport;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
 public class UserOutput {
-    private Inventory inventory = new Inventory();
-    private List<String> list = inventory.getItemList();
-    public void displayItems() {
-        for (String slot : list) {
+//    private Inventory inventory = new Inventory();
+//    private List<String> list = inventory.getItemList();
+    public void displayItems(Inventory inventory) {
+        for (String slot : inventory.getItemList()) {
             String name = inventory.getName(slot);
             BigDecimal price = inventory.getPrice(slot);
 
@@ -20,5 +22,22 @@ public class UserOutput {
 
     public void exit() {
         System.out.println("Thank you for your business. Goodbye!");
+    }
+    public void showCurrentBalance(Bank bank) {
+        System.out.println("Current Money provided: " + bank.getCurrentBalance());
+    }
+
+    public void displaySalesReport(Inventory inventory, SalesReport sales) {
+        for (String slot : inventory.getItemList()) {
+            String name = inventory.getName(slot);
+            boolean wasPurchased = sales.getTotalSales().containsKey(slot);
+
+            if (wasPurchased) {
+                int quantity = sales.getTotalSales().get(slot);
+                System.out.println(inventory.getName(slot) + "|" + quantity);
+            } else {
+                System.out.println(inventory.getName(slot) + "|" + 0);
+            }
+        }
     }
 }
