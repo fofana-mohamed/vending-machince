112d60316ddfc0149c4befb91ec815507eb2035a
Mohamed Fofana <mohamed.fofana63@yahoo.com>
2022-06-30 11:14:10 -0400

    added displayItems and exit methods to UserOutput class

--- java/src/main/java/com/techelevator/UI/UserOutput.java
+++ java/src/main/java/com/techelevator/UI/UserOutput.java
@@ -1,8 +1,24 @@
 package com.techelevator.UI;
 
+import com.techelevator.Inventory.Inventory;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
 public class UserOutput {
-
+    private Inventory inventory = new Inventory();
+    private List<String> list = inventory.getItemList();
     public void displayItems() {
+        for (String slot : list) {
+            String name = inventory.getName(slot);
+            BigDecimal price = inventory.getPrice(slot);
 
+            System.out.println(slot + "|" + name + "|" + price);
+        }
+    }
+
+    public void exit() {
+        System.out.println("Thank you for your business. Goodbye!");
     }
 }
