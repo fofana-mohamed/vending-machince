8a9450f771d534c9abd98f558a2514a744961424
Mohamed Fofana <mohamed.fofana63@yahoo.com>
2022-07-01 09:09:56 -0400

    created class and added colors

---
+++ java/src/main/java/com/techelevator/UI/Color.java
@@ -0,0 +1,25 @@
+package com.techelevator.UI;
+
+public class Color {
+
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_BLACK = "\u001B[30m";
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_GREEN = "\u001B[32m";
+    public static final String TEXT_YELLOW = "\u001B[33m";
+    public static final String TEXT_BLUE = "\u001B[34m";
+    public static final String TEXT_PURPLE = "\u001B[35m";
+    public static final String TEXT_CYAN = "\u001B[36m";
+    public static final String TEXT_WHITE = "\u001B[37m";
+
+    public String getTextReset() {return TEXT_RESET;}
+    public String getTextBlack() {return TEXT_BLACK;}
+    public String getTextRed() {return TEXT_RED;}
+    public String getTextGreen() {return TEXT_GREEN;}
+    public String getTextYellow() {return TEXT_YELLOW;}
+    public String getTextBlue() {return TEXT_BLUE;}
+    public String getTextPurple() {return TEXT_PURPLE;}
+    public String getTextCyan() {return TEXT_CYAN;}
+    public String getTextWhite() {return TEXT_WHITE;}
+
+}
